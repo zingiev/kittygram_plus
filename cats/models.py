4e41aee3272b2f1dfdde3070b42d681452188de8
zingiev <m@zingiev.ru>
2024-10-20 13:45:23 +0300

    1

--- cats/models.py
+++ cats/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 
 
+class Achievement(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
-
+    owner = models.ForeignKey(
+        Owner, related_name='cats', on_delete=models.CASCADE)
+    achievements = models.ManyToManyField(
+        Achievement, through='AchievementCat')
 
     def __str__(self):
         return self.name
+
+
+class AchievementCat(models.Model):
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.achievement} {self.cats}'
--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,41 @@
 from rest_framework import serializers
 
-from .models import Cat
+from .models import Cat, Owner, Achievement, AchievementCat
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    # owner = serializers.StringRelatedField(read_only=True)
+    achievements = AchievementSerializer(many=True, required=False)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement)
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat)
+        return cat
